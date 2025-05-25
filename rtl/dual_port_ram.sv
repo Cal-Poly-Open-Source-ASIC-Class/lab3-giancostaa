ec6c51366cf846ac1cf22333e42127aa64cfdcc8
Giancarlo Acosta <giancarlo_guijarro@yahoo.com>
2025-05-24 21:34:20 -0700

    let the linting begin

--- rtl/dual_port_ram.sv
+++ rtl/dual_port_ram.sv
@@ -7,7 +7,7 @@ module dual_port_ram (
 
     /* Port A Wishbone Signals */
     input  logic          pA_wb_stb_i,
-    input  logic [8 : 0] pA_wb_addr_i,
+    input  logic [8 : 0]  pA_wb_addr_i,
     input  logic [3 : 0]  pA_wb_sel_i,
     input  logic          pA_wb_we_i,
     input  logic [31 : 0] pA_wb_data_i,
@@ -18,7 +18,7 @@ module dual_port_ram (
     
     /* Port B Wishbone Signals */
     input  logic          pB_wb_stb_i,
-    input  logic [8 : 0] pB_wb_addr_i,
+    input  logic [8 : 0]  pB_wb_addr_i,
     input  logic [3 : 0]  pB_wb_sel_i,
     input  logic          pB_wb_we_i,
     input  logic [31 : 0] pB_wb_data_i,
@@ -31,26 +31,27 @@ module dual_port_ram (
     
     /* DFF RAM Signals */
 
-    logic [10 : 0] lo_A0 , hi_A0;
+    logic [7 : 0]  lo_A0 , hi_A0;
     logic [31 : 0] lo_Di0, hi_Di0;
+    logic [31 : 0] lo_Do0, hi_Do0;
     logic          lo_EN0, hi_EN0;
     logic [3 : 0]  lo_WE0, hi_WE0;  
 
     DFFRAM256x32 hi_ram (
         .CLK(clk_i),
         .WE0(hi_WE0),
-        .EN0(pA_en),
+        .EN0(hi_EN0),
         .Di0(hi_Di0),
-        .Do0(pA_wb_data_o),
+        .Do0(hi_Do0),
         .A0(hi_A0)
     );
 
     DFFRAM256x32 lo_ram (
         .CLK(clk_i),
         .WE0(lo_WE0),
-        .EN0(pB_en),
+        .EN0(lo_EN0),
         .Di0(lo_Di0),
-        .Do0(pB_wb_data_o),
+        .Do0(lo_Do0),
         .A0(lo_A0)
     );
 
@@ -67,14 +68,6 @@ module dual_port_ram (
         PORT_A = 1'b0,
         PORT_B = 1'b1
     } port_t;
-
-    typedef enum
-    {
-        INIT,
-        ACK,
-        STALL
-    } state_t;
-
 
     logic collision;
     port_t prio, next_prio;
@@ -94,13 +87,68 @@ module dual_port_ram (
             pB_ram = HI_RAM;
     end
 
-    assign collision = pA_ram == pB_ram; 
+    /* Write Enable logic */
+    logic [3 : 0] pA_WE0, pB_WE0;
+
+    assign pA_WE0 = pA_wb_sel_i & {4{pA_wb_we_i}};
+    assign pB_WE0 = pB_wb_sel_i & {4{pB_wb_we_i}};
+
+    /* Arbitration */
+
+    logic pA_lo_arb, pB_lo_arb, pA_hi_arb, pB_hi_arb;
+    logic pA_lo_arb_reg, pB_lo_arb_reg, pA_hi_arb_reg, pB_hi_arb_reg;
+
+    assign pA_lo_arb = ((collision && prio)  || (!collision && pA_wb_stb_i)) && (pA_ram == LO_RAM);
+    assign pB_lo_arb = ((collision && !prio) || (!collision && pB_wb_stb_i)) && (pB_ram == LO_RAM);
+    
+    assign pA_hi_arb = ((collision && prio)  || (!collision && pA_wb_stb_i)) && (pA_ram == HI_RAM);
+    assign pB_hi_arb = ((collision && !prio) || (!collision && pB_wb_stb_i)) && (pB_ram == HI_RAM);
+
+    assign collision = ((pA_ram == pB_ram) && pA_wb_stb_i && pB_wb_stb_i); 
 
     always_ff @ (posedge clk_i) begin
-        if (rst_n_i == 0)
+        if (rst_n_i == 0) begin
             prio <= PORT_A;
-        else if (collision)
+        end else if (collision)
             prio <= next_prio;
+    end
+
+    always_ff @ (posedge clk_i) begin
+        if (rst_n_i == 0) begin
+            pA_lo_arb_reg <= 0;
+            pA_hi_arb_reg <= 0;
+            pB_lo_arb_reg <= 0;
+            pB_hi_arb_reg <= 0;
+            pA_wb_ack_o   <= 0;
+            pB_wb_ack_o   <= 0;
+        end else begin
+
+            if (pA_lo_arb_reg || pA_hi_arb_reg)
+                pA_wb_ack_o <= 1;
+            else
+                pA_wb_ack_o <= 0;
+
+            if (pB_lo_arb_reg || pB_hi_arb_reg)
+                pB_wb_ack_o <= 1;
+            else
+                pB_wb_ack_o <= 0;
+
+            if (pA_lo_arb_reg && !pB_lo_arb_reg)
+                pA_wb_data_o <= lo_Do0;
+            else if (pA_hi_arb_reg && !pB_hi_arb_reg) 
+                pA_wb_data_o <= hi_Do0;
+
+            if (!pA_lo_arb_reg && pB_lo_arb_reg)
+                pB_wb_data_o <= lo_Do0;
+            else if (!pA_hi_arb_reg && pB_hi_arb_reg)
+                pB_wb_data_o <= hi_Do0;
+        
+
+            pA_lo_arb_reg <= pA_lo_arb;
+            pA_hi_arb_reg <= pA_hi_arb;
+            pB_lo_arb_reg <= pB_lo_arb;
+            pB_hi_arb_reg <= pB_hi_arb;
+        end
     end
 
     always_comb begin
@@ -120,71 +168,52 @@ module dual_port_ram (
 
     end
 
-    /* Write Enable logic */
-    logic [3 : 0] pA_WE0, pB_WE0;
-
-    assign pA_WE0 = pA_wb_sel_i & {4{pA_wb_we_i}};
-    assign pB_WE0 = pB_wb_sel_i & {4{pB_wb_we_i}};
-
-    /* Arbitration */
-
-    logic pA_lo_arbiter, pB_lo_arbiter, pA_hi_arbiter, pB_hi_arbiter;
-
-    assign pA_lo_arbiter = ((collision && prio)  || (!collision)) && (pA_ram == LO_RAM);
-    assign pB_lo_arbiter = ((collision && !prio) || (!collision)) && (pB_ram == LO_RAM);
-    
-    assign pA_hi_arbiter = ((collision && prio)  || (!collision)) && (pA_ram == HI_RAM);
-    assign pB_hi_arbiter = ((collision && !prio) || (!collision)) && (pB_ram == HI_RAM);
-
     always_comb begin
 
-        if (pA_lo_arbiter && pB_lo_arbiter) begin
+        if (pA_lo_arb && pB_lo_arb) begin
             $display("I shouldn't be here...");
-        end else if (pA_hi_arbiter && pB_hi_arbiter) begin
+        end else if (pA_hi_arb && pB_hi_arb) begin
             $display("I shouldn't be here either...");
         end
 
 
         /* Lo RAM Input Arbitration */ 
-        if (pA_lo_arbiter && !pB_lo_arbiter) begin
-            lo_A0  = pA_wb_addr_i;
+        if (pA_lo_arb && !pB_lo_arb) begin
+            lo_A0  = pA_wb_addr_i[7 : 0];
             lo_Di0 = pA_wb_data_i;
             lo_EN0 = pA_wb_stb_i;
             lo_WE0 = pA_WE0;
-        end else if (!pA_lo_arbiter && pB_lo_arbiter) begin
-            lo_A0  = pB_wb_addr_i;
+        end else if (!pA_lo_arb && pB_lo_arb) begin
+            lo_A0  = pB_wb_addr_i[7 : 0];
             lo_Di0 = pB_wb_data_i;
             lo_EN0 = pB_wb_stb_i;
             lo_WE0 = pB_WE0;
         end else begin
-            lo_A0 = {32'hDEADBEEF}[10 : 0];
+            lo_A0 = {32'hDEADBEEF}[7 : 0];
             lo_Di0 = 32'hDEADBEEF;
             lo_EN0 = 0;
             lo_WE0 = 0;
         end
 
          /* Hi RAM Input Arbitration */ 
-        if (pA_hi_arbiter && !pB_hi_arbiter) begin
-            hi_A0  = pA_wb_addr_i;
+        if (pA_hi_arb && !pB_hi_arb) begin
+            hi_A0  = pA_wb_addr_i[7 : 0];
             hi_Di0 = pA_wb_data_i;
             hi_EN0 = pA_wb_stb_i;
             hi_WE0 = pA_WE0;
-        end else if (!pA_hi_arbiter && pB_hi_arbiter) begin
-            hi_A0  = pB_wb_addr_i;
+        end else if (!pA_hi_arb && pB_hi_arb) begin
+            hi_A0  = pB_wb_addr_i[7 : 0];
             hi_Di0 = pB_wb_data_i;
             hi_EN0 = pB_wb_stb_i;
             hi_WE0 = pB_WE0;
         end else begin
-            hi_A0 = {32'hDEADBEEF}[10 : 0];
+            hi_A0 = {32'hDEADBEEF}[7 : 0];
             hi_Di0 = 32'hDEADBEEF;
             hi_EN0 = 0;
             hi_WE0 = 0;
         end
 
-
     end
-
-  
 
 endmodule
 
